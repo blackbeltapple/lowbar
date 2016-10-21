5a66a426d650439a6e97108fe00d8468f8ed7cfc
amanda manekshaw <amanda_manekshaw@hotmail.com>
2016-10-21 16:32:16 +0100

    Add _.last function

--- lowbar.js
+++ lowbar.js
@@ -11,19 +11,42 @@ _.identity = function(value){
 
 };
 
+// _.first function//
+//
 _.first = function(list, item){
+  if(Array.isArray(list)){
+      var result =[];
+      if(item === undefined){
+        item =1;
+      }
 
-  var result =[];
+      for (i=0; i<item; i++){
+        result.push(list[i]);
+      }
+      return result;
 
-  if(item === undefined){
-    item =1;
+  } else {
+
+    return undefined;
   }
-  for (i=0; i<item; i++){
-    result.push(list[i]);
-  }
-  
-  return result;
+};
 
+// _.last function//
+//
+_.last = function(list, item){
+if(Array.isArray(list)){
+    var result = [];
+    if (item === undefined) {
+      item = 1;  // default value if no param given
+    }
+    for (i=list.length - item; i < list.length; i++){
+      result.push(list[i]);
+      }
+      return result;
+
+      } else {
+      return undefined;
+    }
 };
 
 
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -20,41 +20,62 @@ describe('_', function () {
 
 //testing #identity
 
-describe('#identity', function(){
-  it('is a function', function(){
-    expect(_.identity).to.be.a('function');
+  describe('#identity', function(){
+    it('is a function', function(){
+      expect(_.identity).to.be.a('function');
+    });
+    it('returns 23 when given 23', function(){
+      expect(_.identity(23)).to.equal(23);
+    });
+    it('returns an object when passed an object', function(){
+      expect(_.identity({name:'joe'})).to.be.an('object');
+      expect(_.identity({name:'joe'})).to.have.property('name');
+    });
+    it('run test code', function(){
+      var stooge = {name: 'moe'};
+      var result = stooge === _.identity(stooge);
+      expect(result).to.equal(true);
+    });
   });
-  it('returns 23 when given 23', function(){
-    expect(_.identity(23)).to.equal(23);
-  });
-  it('returns an object when passed an object', function(){
-    expect(_.identity({name:'joe'})).to.be.an('object');
-    expect(_.identity({name:'joe'})).to.have.property('name');
-  });
-  it('run test code', function(){
-    var stooge = {name: 'moe'};
-    var result = stooge === _.identity(stooge);
-    expect(result).to.equal(true);
-  });
-});
 
-// testing _.first function
+  // testing _.first function
 
-describe('#first', function(){
- it('is a function', function(){
-   expect(_.first).to.be.a('function');
- });
-it('it accepts 1 or 2 arguments', function(){
-  expect(_.first.length).to.be.within(1,2);
-});
-it('returns an array', function(){
-  expect(_.first([])).to.be.a('array');
-})
-it('returns the expected values from a the head of given array', function(){
-  expect(_.first([4,6,7,8,9], 3)).to.eql([4,6,7]);
-  expect(_.first([4,6,7,8,9])).to.eql([4,6]);
-});
-});
+  describe('#first', function(){
+   it('is a function', function(){
+     expect(_.first).to.be.a('function');
+   });
+   it('it accepts 1 or 2 arguments', function(){
+    expect(_.first.length).to.be.within(1,2);
+    });
+    it('returns an array', function(){
+      expect(_.first([])).to.be.a('array');
+    });
+    it('returns the expected values from a the head of given array', function(){
+      expect(_.first([4,6,7,8,9], 3)).to.eql([4,6,7]);
+      expect(_.first([4,6,7,8,9])).to.eql([4]);
+    });
+    it('receives no parameters, it returns undefined', function(){
+      expect(_.first()).to.be.undefined;
+    });
+  });
 
+  describe('#last', function(){
+   it('is a function', function(){
+     expect(_.last).to.be.a('function');
+   });
+   it('it accepts 1 or 2 arguments', function(){
+    expect(_.last.length).to.be.within(1,2);
+    });
+    it('returns an array', function(){
+      expect(_.last([])).to.be.a('array');
+    });
+    it('returns the expected values from a the head of given array', function(){
+      expect(_.last([4,6,7,8,9], 3)).to.eql([7,8,9]);
+      expect(_.last([4,6,7,8,9])).to.eql([9]);
+    });
+    it('receives no parameters, it returns undefined', function(){
+      expect(_.last()).to.be.undefined;
+    });
+  });
 
 });
